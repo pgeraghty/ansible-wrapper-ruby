b1356e10a98b1f0aa73dcfdee726ac7815a52a8f
pg <muse@appsthatcould.be>
2015-07-11 02:07:39 +0200

    Skip host checking for tests

--- spec/ansible/ad_hoc_spec.rb
+++ spec/ansible/ad_hoc_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Ansible
   describe AdHoc do
     it 'can execute a basic ad-hoc Ansible command on localhost' do
-      expect(AdHoc.run 'all -i localhost, --list-hosts').to match /localhost/
+      expect(AdHoc.run 'all -i localhost, --list-hosts', skip_host_key_checking: true).to match /localhost/
     end
   end
 end
--- spec/ansible/playbook_spec.rb
+++ spec/ansible/playbook_spec.rb
@@ -1,19 +1,19 @@
 module Ansible
   describe Playbook do
     it 'can execute a basic Ansible Playbook command on localhost' do
-      expect(Playbook.run '-i localhost, spec/mock_playbook.yml --list-hosts').to match /localhost/
+      expect(Playbook.run '-i localhost, spec/mock_playbook.yml --list-hosts', skip_host_key_checking: true).to match /localhost/
     end
 
     it 'can execute a basic Ansible Playbook' do
-      expect(Playbook.run '-i localhost, spec/mock_playbook.yml').to match /TASK: \[Test task\]/
+      expect(Playbook.run '-i localhost, spec/mock_playbook.yml', skip_host_key_checking: true).to match /TASK: \[Test task\]/
     end
 
     it 'can stream the output from execution of an Ansible Playbook' do
       cmd = '-i localhost, spec/mock_playbook.yml'
 
       expect { |b| Playbook.stream cmd, &b }.to yield_control
-      expect(Playbook.stream(cmd) { |l| next }).to be_a Fixnum
-      expect(Playbook.stream(cmd) { |l| break l if l[/PLAY \[Testing Playbook\]/] }).to match /Testing Playbook/
+      expect(Playbook.stream(cmd, skip_host_key_checking: true) { |l| next }).to be_a Fixnum
+      expect(Playbook.stream(cmd, skip_host_key_checking: true) { |l| break l if l[/PLAY \[Testing Playbook\]/] }).to match /Testing Playbook/
     end
   end
 end
