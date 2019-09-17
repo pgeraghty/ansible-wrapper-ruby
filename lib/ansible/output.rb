275ca128b65b392e5bb47566a2a217a204e47754
Paul Geraghty <pgeraghty07@gmail.com>
2019-09-17 23:06:01 +0200

    Tweak Output module, handle bold prefix

--- lib/ansible/output.rb
+++ lib/ansible/output.rb
@@ -3,23 +3,36 @@ require 'erb'
 
 module Ansible::Output
   COLOR = {
-  '1' => 'font-weight: bold',
-  '30' => 'color: black',
-  '31' => 'color: red',
-  '32' => 'color: green',
-  '33' => 'color: yellow',
-  '34' => 'color: blue',
-  '35' => 'color: magenta',
-  '36' => 'color: cyan',
-  '37' => 'color: white',
-  '90' => 'color: grey'
+    '1' => 'font-weight: bold',
+    '30' => 'color: black',
+    '31' => 'color: red',
+    '32' => 'color: green',
+    '33' => 'color: yellow',
+    '34' => 'color: blue',
+    '35' => 'color: magenta',
+    '36' => 'color: cyan',
+    '37' => 'color: white',
+    '90' => 'color: grey'
   }
 
   def self.to_html(line, stream='')
     s = StringScanner.new(ERB::Util.h line)
     while(!s.eos?)
-      if s.scan(/\e\[0;(3[0-7]|90|1)m/)
-        stream << %{<span style="#{COLOR[s[1]]};">}
+      if s.scan(/\e\[([0-1]);(3[0-7]|90|1)m/)
+        bold, colour = s.captures
+        styles = []
+
+        styles << COLOR[bold] if bold.to_i == 1
+        styles << COLOR[colour]
+
+        span =
+          if styles.empty?
+            %{<span>}
+          else
+            %{<span style="#{styles*'; '};">}
+          end
+
+        stream << span
       else
         if s.scan(/\e\[0m/)
           stream << %{</span>}
@@ -28,7 +41,6 @@ module Ansible::Output
         end
       end
     end
-
 
     stream
   end
